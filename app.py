f7e4f412d776c51461d900bc2a4eb7fb184c94b7
Muhammad Qadir <mahdimq@gmail.com>
2020-08-13 16:31:28 +0300

    add environ vars

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///foodie'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'secret_recipe'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret_recipe')
 
 connect_db(app)
 
